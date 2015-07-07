aa2e200e969670a6afa39d0debb03cafe2550915
Damian Ivereigh <damian@launtel.net.au>
2015-07-07 23:36:07 +1000

    Brought GIT up to version 7.4 from Niels Dettenbach

--- Speech/Google/TTS.pm
+++ Speech/Google/TTS.pm
@@ -10,6 +10,66 @@ package Speech::Google::TTS;
 # the GNU General Public License Version 2.
 #
 
+=head1 NAME
+
+Speech::Google::TTS - Module that uses Google Translate for text to speech synthesis.!
+
+=head1 VERSION
+
+Version 0.74
+
+=cut
+
+our $VERSION = '0.74';
+
+
+=head1 SYNOPSIS
+
+This Module uses Googles Translate Service for text to multilingual
+text to speech synthesis.
+
+This Module allows to use the Google TTS service behind the
+Google Translation service from Perl and is the core of the
+following Speech::Google Text to Speech Module set.
+
+Perhaps a little code snippet.
+
+    	use Speech::Google::TTS;
+
+    	my $a = Speech::Google::TTS->new();
+
+        $a = new Speech::Google::TTS();
+
+        $a->say_text("speak this text!");
+        $a->as_filename();
+
+        $a->{'lang'} = 'de';
+        $a->say_text("sprich diesen Text");
+        $a->as_filename();
+    	...
+
+=head1 FUNCTIONS
+
+=head2 new
+
+Create a new Speech::Google::TTS object and initialize configuration.
+
+=head2 say_text
+
+Generates a WAV or MP3 from text, stored in the temp directory.
+
+=head2 as_filename
+
+Gives back the filename of the last generated WAV or MP3 from i.e. "say_text".
+
+=head2 languages
+
+The list of currently supported languages.
+
+=cut
+
+
+
 use warnings;
 use strict;
 use File::Temp qw(tempfile tempdir);
@@ -28,7 +88,7 @@ sub new {
         $self->{'samplerate'}           = 16000;
         $self->{'speed'}                = 1.2;
         $self->{'lang'}                 = "en";
-        $self->{'tmpdir'}        	= tempdir( CLEANUP =>  0 );;
+        $self->{'tmpdir'}        	= tempdir( CLEANUP =>  0 );
         $self->{'timeout'}    		= "10";
         $self->{'googleurl'} 		= "http://translate.google.com/translate_tts";
         $self->{'languages'}		= languages();
@@ -214,4 +274,69 @@ sub languages {
 	return %sup_lang;
 }
 
-1;
+
+=head1 AUTHOR
+
+Niels Dettenbach, <nd@syndicat.com> http://www.syndicat.com
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-speech-google-tts at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Speech::Google::TTS>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+Feel free to visit the project website http://www.syndicat.com/open_source/google/perl/googletts/
+
+or write to googletts@syndicat.com
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Speech::Google::TTS
+
+
+Feel free to visit the project website http://www.syndicat.com/open_source/google/perl/googletts/
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Speech::Google::TTS>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Speech::Google::TTS>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Speech::Google::TTS>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Speech::Google::TTS>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2012/2013 Niels Dettenbach <nd@syndicat.com>, 
+with contributes of Lefteris Zafiris <zaf.000@gmail.com>
+all rights reserved.
+
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+
+=cut
+
+1; # End of Speech::Google::TTS
+
+
